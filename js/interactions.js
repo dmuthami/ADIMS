c045da4161b4bafa11508f62f34f052c3aa79136
dmuthami <waruid@gmail.com>
2017-02-20 00:53:04 +0300

    Version 1.0.1
    Included a template
    
    Worked with the DB

--- js/interactions.js
+++
@@ -1,8 +0,0 @@
-//interactions
-
-// create an interaction to add to the map that isn't there by default
-var interaction = new ol.interaction.DragRotateAndZoom();
-
-$(document).ready(function () {
-	map.addInteraction(interaction);
-});
--- js/map.js
+++ js/map.js
@@ -1,60 +1,141 @@
-var extent = [4091020.256326, -15468.942551, 4099588.155744, -11577.705952];
-
-var format = 'image/png';
-
-var wmsSource = new ol.source.ImageWMS({
-    url: 'http://localhost:8080/geoserver/adims/wms',
-    params: {
-        'FORMAT': format,
-        'VERSION': '1.1.1',
-        tiled: true,
-        STYLES: 'farmers3',
-        LAYERS: 'adims:farmers3_evw'
-    },
-    serverType: 'geoserver'
-    //,crossOrigin: 'anonymous'
-});
-
-var osmLayer = new ol.layer.Tile({
-    source: new ol.source.OSM()
-});
-
-var layers = [osmLayer,
-	new ol.layer.Image({
-	    source: wmsSource
-	})
-];
-
-var view = new ol.View({
-    center: ol.proj.transform([36.750, -0.123], 'EPSG:4326', 'EPSG:3857'),
-    extent: extent,
-    zoom: 14
-});
-/**
- * Create the map.
+﻿
+/***
+ * global Variabes
+ * 
  */
-var map = new ol.Map({
-    layers: layers,
-    //overlays: [overlay],
-    target: 'map',
-    controls: [
-		new ol.control.Zoom(),
-		new ol.control.FullScreen(),
-		new ol.control.MousePosition(),
-		new ol.control.ScaleLine(),
-		new ol.control.ZoomSlider(),
-		new ol.control.ZoomToExtent(),
-		new ol.control.Rotate(),
-		new ol.control.OverviewMap()
-    ],
-    view: view
-});
-
+var extent, format, wmsSource, osmLayer, layers, view;
+var map;
 /**
- * Add a click handler to the map to render the popup.
+ * End of global variables
  */
-map.on('singleclick', function (evt) {
-    //Call function to renderer popup was a single click event is fired up
-    displayInfoFeatures(evt);
-});
+function applyMargins() {
+    var leftToggler = $(".mini-submenu-left");
+    if (leftToggler.is(":visible")) {
+        $("#map .ol-zoom")
+          .css("margin-left", 0)
+          .removeClass("zoom-top-opened-sidebar")
+          .addClass("zoom-top-collapsed");
+    } else {
+        $("#map .ol-zoom")
+          .css("margin-left", $(".sidebar-left").width())
+          .removeClass("zoom-top-opened-sidebar")
+          .removeClass("zoom-top-collapsed");
+    }
+}
 
+function isConstrained() {
+    return $(".sidebar").width() == $(window).width();
+}
+
+function applyInitialUIState() {
+    if (isConstrained()) {
+        $(".sidebar-left .sidebar-body").fadeOut('slide');
+        $('.mini-submenu-left').fadeIn();
+    }
+}
+
+function instantiateGlobalVariables() {
+    //An array of numbers representing an extent: [minx, miny, maxx, maxy]
+    //extent = [4091020.256326, -15468.942551, 4099588.155744, -11577.705952]; //for farmers
+
+    extent = [-919335.054, -421034.721, 5489455.831, 4323205.57]; //farmcoordinate_to_farmer_evw
+
+    format = 'image/png';
+
+    wmsSource = new ol.source.ImageWMS({
+        url: 'http://localhost:8080/geoserver/adims/wms',
+        params: { 'LAYERS': 'adims:farmcoordinate_to_farmer_evw' },
+        //params: {
+        //    'FORMAT': format,
+        //    'VERSION': '1.1.1',
+        //    tiled: true,
+        //    STYLES: 'farmers3',
+        //    LAYERS: 'adims:farmcoordinate_to_farmer_evw'
+        //},
+        serverType: 'geoserver'
+        //,crossOrigin: 'anonymous'
+    });
+
+    osmLayer = new ol.layer.Tile({
+        source: new ol.source.OSM()
+    });
+
+    layers = [osmLayer,
+        new ol.layer.Image({
+            source: wmsSource
+        })
+    ];
+
+    view = new ol.View({
+        center: ol.proj.transform([36.750, -0.123], 'EPSG:4326', 'EPSG:3857'),
+        extent: extent,
+        zoom: 7
+    });
+};
+
+function popupFunctions() {
+    //Add overlay
+    map.addOverlay(overlay);
+
+    //Wire singleclick event to dispayfearuresinfo handler
+
+
+    /**
+     * Add a click handler to the map to render the popup.
+     */
+    map.on('singleclick', function (evt) {
+        //Call function to renderer popup was a single click event is fired up
+        displayInfoFeatures(evt);
+
+
+    });
+}
+
+
+$(function () {
+    $('.sidebar-left .slide-submenu').on('click', function () {
+        var thisEl = $(this);
+        thisEl.closest('.sidebar-body').fadeOut('slide', function () {
+            $('.mini-submenu-left').fadeIn();
+            applyMargins();
+        });
+    });
+
+    $('.mini-submenu-left').on('click', function () {
+        var thisEl = $(this);
+        $('.sidebar-left .sidebar-body').toggle('slide');
+        thisEl.hide();
+        applyMargins();
+    });
+
+    $(window).on("resize", applyMargins);
+
+    //call function to instantiate map objects
+    instantiateGlobalVariables();
+
+    /**
+     * Create the map.
+     */
+    map = new ol.Map({
+        layers: layers,
+        controls: [
+            new ol.control.Zoom(),
+            new ol.control.FullScreen(),
+            //new ol.control.MousePosition(),
+            new ol.control.ScaleLine(),
+            //new ol.control.ZoomSlider(),
+            //new ol.control.ZoomToExtent(),
+            new ol.control.Rotate(),
+            new ol.control.OverviewMap()
+        ],
+        //overlays: [overlay],
+        target: 'map',
+        view: view
+    });
+
+    //Call map related components
+    popupFunctions();
+
+    applyInitialUIState();
+    applyMargins();
+});
